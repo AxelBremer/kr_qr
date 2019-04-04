b173f45ac27f00f7ccfcb2b79981a677b29d9396
AxelBremer <axelbremer94@gmail.com>
2019-04-04 16:23:44 +0200

    vgm klopt ie

--- QR.py
+++ QR.py
@@ -7,6 +7,9 @@ os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 state_counter = 0
 
+d_name = {0:'0', -1:'-', 1:'+'}
+q_name = {0:'0', 1:'+', 2:'Max'}
+
 def next_iq(params):
     """
     Returns all possible next values of iq given the current parameters.
@@ -21,7 +24,21 @@ def next_iq(params):
         possible.append(0)
     
     return possible
-    
+
+def next_id(params):
+    possible = []
+    if params['ID'] == 1:
+        if params['VQ'] == 2:
+            possible += [0]
+        else:
+            possible += [1]
+    if params['ID'] == 0:
+        possible = possible + [-1,0]
+    if params['ID'] == -1:
+        possible = possible + [-1,0]
+
+    return possible
+
 def next_vq(params):
     """
     Returns all possible next values of vq given the current parameters.
@@ -48,12 +65,12 @@ def next_vq(params):
         # 0 stays 0
         if params['VQ'] == 0:
             possible.append(0)
-        # 1 becomes 1 or 2
+        # 1 becomes 0 or 1
         elif params['VQ'] == 1:
             possible = possible + [0,1]
-        # 2 becomes 1
+        # 2 becomes 1 or 2
         elif params['VQ'] == 2:
-            possible.append(1)
+            possible = possible + [1,2]
     
     return possible
     
@@ -139,36 +156,32 @@ def get_states():
     combs = [dict(zip(s, v)) for v in product(*s.values())]
     toremove = []
     for s in combs:
+        # exogenous behaviour: only turn tap at start
+        if s['ID'] == 1 and s['IQ'] == 0 and s['VQ'] != 0:
+            if s not in toremove:
+                toremove.append(s)
         if s['VQ'] != s['OQ']:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(1)
             if s not in toremove:
                 toremove.append(s)
         if s['VD'] != s['OD']:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(2)
             if s not in toremove:
                 toremove.append(s)
-        if s['IQ'] == 0 and s['ID'] != 0:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(3)
+        if s['IQ'] == 0 and s['ID'] == -1:
             if s not in toremove:
                 toremove.append(s)
-        if s['VQ'] == 0 and s['VD'] != 0:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(4)
+        if s['VQ'] == 0 and s['VD'] == -1:
             if s not in toremove:
                 toremove.append(s)
-        if s['OQ'] == 0 and s['OD'] != 0:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(5)
+        if s['OQ'] == 0 and s['OD'] == -1:
             if s not in toremove:
                 toremove.append(s)
         if s['IQ'] == 0 and s['VD'] == 1:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(6)
             if s not in toremove:
                 toremove.append(s)
         if s['IQ'] == 0 and s['OQ'] > 0 and s['VD'] != -1:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(7)
             if s not in toremove:
                 toremove.append(s)
         if s['IQ'] == 1 and s['OQ'] == 0 and s['VD'] != 1:
-            if s == {'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0}: print(8)
             if s not in toremove:
                 toremove.append(s)
 
@@ -184,28 +197,26 @@ def get_transitions(states):
     return transitions
 
 def plausible_transitions(states, transitions):
-	toremove = []
+    toremove = []
 
-	for i in transitions:
-		prev_state = states[i[0]]
-		successor_iqs = next_iq(prev_state)
-		successor_vqs = next_vq(prev_state)
-		next_state = states[i[1]]
-		
-		if(next_state['IQ'] not in successor_iqs or next_state['VQ'] not in successor_vqs):
-			toremove.append(i)
-		'''
-		else:
-			successor_vds = next_vd(next_state)
-			print(successor_vds)
-			if(next_state['VD'] not in successor_vds):
-				print(next_state['VD'])
-				toremove.append(i)
-		'''
-	for s in toremove:
-		transitions.remove(s)
-	print("States left after plausible_transitions: ",len(transitions))
-	return transitions
+    for i in transitions:
+        prev_state = states[i[0]]
+        next_state = states[i[1]]
+        
+        if(next_state['IQ'] not in next_iq(prev_state)):
+            if i not in toremove:
+                toremove.append(i)
+        if(next_state['VQ'] not in next_vq(prev_state)):
+            if i not in toremove:
+                toremove.append(i)
+        if(next_state['ID'] not in next_id(prev_state)):
+            if i not in toremove:
+                toremove.append(i)
+
+    for s in toremove:
+        transitions.remove(s)
+    print("Transitions left after plausible_transitions: ",len(transitions))
+    return transitions
 
 def is_instant(prev, new):
     if prev != new:
@@ -233,23 +244,33 @@ def epsilon_ordering(states, transitions):
 
     for t in toremove:
         transitions.remove(t)
-    print("States left after epsilon_ordering: ",len(transitions))
+    print("Transitions left after epsilon_ordering: ",len(transitions))
     return transitions
 
 def state_to_string(i,s):
     string = str(i)
-    string += "\nInflow: (" + str(s['ID']) + ", " + str(s['IQ']) + ")\n"
-    string += "Volume: (" + str(s['VD']) + ", " + str(s['VQ']) + ")\n"
-    string += "Outflow: (" + str(s['OD']) + ", " + str(s['OQ']) + ")\n"
+    string += "\nInflow: (d:" + d_name[s['ID']] + ", q:" + q_name[s['IQ']] + ")\n"
+    string += "Volume: (d:" + d_name[s['VD']] + ", q:" + q_name[s['VQ']] + ")\n"
+    string += "Outflow: (d:" + d_name[s['OD']] + ", q:" + q_name[s['OQ']] + ")\n"
     return string
 
 def create_graph(states, transitions):
     dot = Digraph(comment='Container')
+    dot.node('r2', label='Start by turning on tap')
+    
     for i,s in enumerate(states):
         dot.node(str(i), state_to_string(i,s))
+
+    dot.edge('r2', '17')
     
     for t in transitions:
         dot.edge(str(t[0]), str(t[1]))
+
+    # for i,s in enumerate(states):
+    #     if i != 17:
+    #         dot.edge(last, str(i))
+    #     last = str(i)
+
     
     dot.render('test-output/container')
     return 
@@ -257,12 +278,11 @@ def create_graph(states, transitions):
 def main():
     states = get_states()
     print('Number of states:',len(states))
-    print({'ID':1, 'IQ':0, 'VD':0, 'VQ':0, 'OD':0, 'OQ':0} in states)
     transitions = get_transitions(states)
-    possible_transitions = plausible_transitions(states, transitions)
-    epsilon_transitions = epsilon_ordering(states, transitions)
+    transitions = plausible_transitions(states, transitions)
+    transitions = epsilon_ordering(states, transitions)
 
-    create_graph(states, epsilon_transitions)
+    create_graph(states, transitions)
 
 if __name__ == '__main__':
     main()
