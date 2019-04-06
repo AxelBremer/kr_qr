a183011dbce20904d3521f5ea966de8ce2f9e020
AxelBremer <axelbremer94@gmail.com>
2019-04-06 17:54:51 +0200

    comments & cleanup

--- QR.py
+++ QR.py
@@ -1,3 +1,9 @@
+'''
+Axel Bremer: 2656186
+Rochelle Choenni:
+Creates a state transition graph by using qualitative reasoning about a container system.
+Dependencies: "pip install graphviz" and "sudo apt-get install graphviz"
+'''
 from itertools import product
 from itertools import permutations
 from graphviz import Digraph
@@ -16,9 +22,9 @@ def next_iq(params):
     """
     possible = []
     if params['ID'] == 0:
-        possible.append(params['IQ'])
+        possible = [params['IQ']]
     if params['ID'] == 1:
-        possible.append(1)
+        possible = [1]
     if params['ID'] == -1:
         if params['IQ'] == 1:
             possible = possible + [0,1]
@@ -36,14 +42,14 @@ def next_id(params):
     # If the container is full you can stop opening the tap.
     if params['ID'] == 1:
         if params['VQ'] == 2:
-            possible += [0]
+            possible = [0]
         else:
-            possible += [1]
+            possible = [1]
     # Afther that the tap can be kept running or be closed more. But never get opened again.
     if params['ID'] == 0:
-        possible = possible + [-1,0]
+        possible = [-1,0]
     if params['ID'] == -1:
-        possible = possible + [-1,0]
+        possible = [-1,0]
 
     return possible
 
@@ -55,30 +61,30 @@ def next_vq(params):
     possible = []
     # if the derivative was 0, q stays the same
     if params['VD'] == 0:
-        possible.append(params['VQ'])
+        possible = [params['VQ']]
         
     # If the derivative was plus
     elif params['VD'] == 1:
         # 0 becomes 1
         if params['VQ'] == 0:
-            possible.append(1)
+            possible = [1]
         # 1 becomes 1 or 2
         elif params['VQ'] == 1:
-            possible = possible + [1,2]
+            possible = [1,2]
         # 2 stays 2
         elif params['VQ'] == 2:
-            possible.append(2)
+            possible = [2]
     
     elif params['VD'] == -1:
         # 0 stays 0
         if params['VQ'] == 0:
-            possible.append(0)
+            possible = [0]
         # 1 becomes 0 or 1
         elif params['VQ'] == 1:
-            possible = possible + [0,1]
+            possible = [0,1]
         # 2 becomes 1 or 2
         elif params['VQ'] == 2:
-            possible = possible + [1,2]
+            possible = [1,2]
     
     return possible
     
@@ -92,74 +98,25 @@ def next_vd(new_params):
     if(new_params['IQ'] == 0):
         # With outflow is negative vd
         if(new_params['OQ'] in [1,2]):
-            possible.append(-1)
+            possible = [-1]
         # No outflow is zero vd
         else:
-            possible.append(0)
+            possible = [0]
     
     # If there is inflow
     elif(new_params['IQ'] == 1):
         # without outflow, vd is 1
         if(new_params['OQ'] == 0):
-            possible.append(1)
+            possible = [1]
             
     
     return possible
-    
-#print(next_vq(s.get_all_params()))
-
-
-
-
-def successor_states(graph, stack, state, infd):
-    
-    params = state.get_all_params()
-    state_counter = state.id+1
-
-    new_state = State({'ID':0,'IQ':0,'VD':0,'VQ':0,'OD':0,'OQ':0}, 0)
-    new_state.params['ID'] = infd
-
-    successor_iqs = next_iq(params)
-    successor_vqs = next_vq(params)
-
-    for iq  in successor_iqs:
-        new_state.params['IQ'] = iq
-        for vq in successor_vqs:
-          
-            new_state.params['VQ'] = vq
-            new_state.params['OQ'] = vq
-
-            successor_vds = next_vd(new_state.params)
-
-            for vd in successor_vds:
-                new_state.params['VD'] = vd
-                new_state.params['OD'] = vd
-
-
-                if new_state == state:
-                    continue
-                else:
-                    if new_state not in graph[state]:
-                        if new_state in graph.keys():
-                            if state in graph[new_state]:
-                                continue
-                        new_state.id = state_counter
-                        graph[state] += [new_state]
-                        state_counter += 1
-                        stack.append(new_state)
-
-
-
-def add_successor_states(graph, searched, stack, infd):
-    while len(stack) > 0:
-        state = stack.pop()
-        if state not in searched:
-            searched.append(state)
-            if state not in graph.keys():
-                graph[state] = []
-            successor_states(graph, stack, state, infd)
 
 def get_states():
+    '''
+    First generates each possible state given the quantity and derivative space. It then truncates
+    the state space by deleting impossible states given the dependencies and exogenous behaviour.
+    '''
     s = {'ID':[-1,0,1],'IQ':[0,1],'VD':[-1,0,1],'VQ':[0,1,2],'OD':[-1,0,1],'OQ':[0,1,2]}
     combs = [dict(zip(s, v)) for v in product(*s.values())]
     toremove = []
@@ -168,12 +125,15 @@ def get_states():
         if s['ID'] == 1 and s['IQ'] == 0 and s['VQ'] != 0:
             if s not in toremove:
                 toremove.append(s)
+        # correspondence of max and 0 of volume and outflow
         if s['VQ'] != s['OQ']:
             if s not in toremove:
                 toremove.append(s)
+        # proportionality volume to outflow
         if s['VD'] != s['OD']:
             if s not in toremove:
                 toremove.append(s)
+        # The derivative cannot be -1 if the quantity is 0
         if s['IQ'] == 0 and s['ID'] == -1:
             if s not in toremove:
                 toremove.append(s)
@@ -183,12 +143,15 @@ def get_states():
         if s['OQ'] == 0 and s['OD'] == -1:
             if s not in toremove:
                 toremove.append(s)
+        # Direct influence: Volume derivative cannot be 1 if inflow quantity is 0
         if s['IQ'] == 0 and s['VD'] == 1:
             if s not in toremove:
                 toremove.append(s)
+        # Direct influence: Volume derivative has to be -1 if outflow is positive and there is no inflow
         if s['IQ'] == 0 and s['OQ'] > 0 and s['VD'] != -1:
             if s not in toremove:
                 toremove.append(s)
+        # Direct influence: Volume derivative has to be 1 if inflow is positive and there is no outflow
         if s['IQ'] == 1 and s['OQ'] == 0 and s['VD'] != 1:
             if s not in toremove:
                 toremove.append(s)
@@ -199,41 +162,44 @@ def get_states():
     return combs
 
 def get_transitions(states):
+    '''
+    Generates all possible state transitions and then deletes the ones that are impossible.
+    '''
     numstates = len(states)
     state_ids = list(range(0, numstates))
     transitions = list(permutations(state_ids, 2))
+    transitions = plausible_transitions(states, transitions)
+    transitions = epsilon_ordering(states, transitions)
     return transitions
 
 def plausible_transitions(states, transitions):
+    '''
+    Removes the impossible transitions by checking the iq, vq and id value transitions.
+    '''
     toremove = []
 
     for i in transitions:
         prev_state = states[i[0]]
         next_state = states[i[1]]
-        
-        if i == (4,5):
-            print(prev_state)
-            print(next_state)
 
         if(next_state['IQ'] not in next_iq(prev_state)):
             if i not in toremove:
-                if i == (4,5): print(1)
                 toremove.append(i)
         if(next_state['VQ'] not in next_vq(prev_state)):
             if i not in toremove:
-                if i == (4,5): print(2)
                 toremove.append(i)
         if(next_state['ID'] not in next_id(prev_state)):
             if i not in toremove:
-                if i == (4,5): print(3)
                 toremove.append(i)
 
     for s in toremove:
         transitions.remove(s)
-    print("Transitions left after plausible_transitions: ",len(transitions))
     return transitions
 
 def is_instant(prev, new):
+    '''
+    Returns if a certain transition is instant or not.
+    '''
     if prev != new:
         if new == 1:
             return 'instant'
@@ -242,6 +208,10 @@ def is_instant(prev, new):
     return 'neither'
 
 def epsilon_ordering(states, transitions):
+    '''
+    Removes every state transition that does not follow epsilon ordering.
+    If a transition has both an instant and a not instant transition it is impossible.
+    '''
     toremove = []
 
     for t in transitions:
@@ -259,10 +229,13 @@ def epsilon_ordering(states, transitions):
 
     for t in toremove:
         transitions.remove(t)
-    print("Transitions left after epsilon_ordering: ",len(transitions))
+    print("Transitions: ",len(transitions))
     return transitions
 
 def state_to_string(i,s):
+    '''
+    Prints the state in a readable format.
+    '''
     string = str(i)
     string += "\nInflow: (d:" + d_name[s['ID']] + ", q:" + q_name[s['IQ']] + ")\n"
     string += "Volume: (d:" + d_name[s['VD']] + ", q:" + q_name[s['VQ']] + ")\n"
@@ -270,6 +243,9 @@ def state_to_string(i,s):
     return string
 
 def create_graph(states, transitions):
+    '''
+    Creates the actual dot graph and saves it to disk.
+    '''
     dot = Digraph(comment='Container')
     dot.node('r2', label='Start by turning on tap')
     
@@ -289,9 +265,6 @@ def main():
     states = get_states()
     print('Number of states:',len(states))
     transitions = get_transitions(states)
-    transitions = plausible_transitions(states, transitions)
-    transitions = epsilon_ordering(states, transitions)
-
     create_graph(states, transitions)
 
 if __name__ == '__main__':
